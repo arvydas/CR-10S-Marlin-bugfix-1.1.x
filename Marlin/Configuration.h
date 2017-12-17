68151779280ccffed634255d10e77a1b8bac9cd6
Arvydas Juskevicius <arvydas@agileinnovative.co.uk>
2017-12-17 13:32:46 +0000

    Size parameters for CR-10S 500mm

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -780,8 +780,8 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 200
-#define Y_BED_SIZE 200
+#define X_BED_SIZE 500
+#define Y_BED_SIZE 500
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
@@ -789,7 +789,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 200
+#define Z_MAX_POS 500
 
 /**
  * Software Endstops
