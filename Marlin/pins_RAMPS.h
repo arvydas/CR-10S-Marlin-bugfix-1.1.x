efce023ee6b751195feb937dc969e6699d4c68ae
Arvydas Juskevicius <arvydas@agileinnovative.co.uk>
2017-12-17 13:27:06 +0000

    Specify correct filament runout sensor

--- Marlin/pins_RAMPS.h
+++ Marlin/pins_RAMPS.h
@@ -201,7 +201,7 @@
 #endif
 
 // define digital pin 4 for the filament runout sensor. Use the RAMPS 1.4 digital input 4 on the servos connector
-#define FIL_RUNOUT_PIN      4
+#define FIL_RUNOUT_PIN      2
 
 #ifndef PS_ON_PIN
   #define PS_ON_PIN        12
